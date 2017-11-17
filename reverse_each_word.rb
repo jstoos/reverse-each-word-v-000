60c4df8d8353a2536a81535cd594e8384a908f7c
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-17 15:11:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split
   word_array.each do |word|
     word.reverse
-    word_array
+    return word_array
   end
   sentence = word_array.join(" ")
   sentence
